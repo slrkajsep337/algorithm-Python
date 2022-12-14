59a62e5eb6a4940632315e05d9afddd882de21bf
이수진 <slrkajsep337@naver.com>
2022-12-14 23:30:07 +0900

    프로그래머스 - 문자열 내 마음대로 정렬하기

--- src/programmers/lev.1/sortStrings.py
+++ src/programmers/lev.1/sortStrings.py
@@ -1,19 +1,8 @@
-#
-#
-# # #문자열 내 마음대로 정렬하기
-#
-#
-# def solution(strings, n):
-#     answer = []
-#     atemp = []
-#
-#     for i in strings:
-#         btemp = []
-#         for j in i: btemp.append(j)
-#         atemp.append(btemp)
-#
-#
-#
-#     return answer
+
+
+# #문자열 내 마음대로 정렬하기
+
+def solution(strings, n):
+    return sorted(strings, key=lambda x:(x[n], x))
 
 
