f64210253ff96e95dcc64c681a440c9c09f8b1c3
이수진 <slrkajsep337@naver.com>
2023-01-01 23:33:55 +0900

    프로그래머스 - 타겟 넘버

---
+++ src/programmers/lev.2/targetNumber.py
@@ -0,0 +1,20 @@
+
+
+
+#타겟넘버 BFS/DFS
+
+#재귀 풀이 -> DFS
+def solution(numbers, target):
+    n = len(numbers)
+    answer = 0
+    def dfs(idx, result):
+        if idx == n:
+            if result ==target:
+                nonlocal answer
+                answer += 1
+            return
+        else:
+            dfs(idx+1, result+numbers[idx])
+            dfs(idx+1, result-numbers[idx])
+    dfs(0, 0)
+    return answer
