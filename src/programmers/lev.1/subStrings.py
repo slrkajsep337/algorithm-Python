bb1437d5424d3ec7be98b487be330b45df0c1164
이수진 <slrkajsep337@naver.com>
2023-01-13 22:02:32 +0900

    프로그래머스 - 크기가 작은 부분문자열

---
+++ src/programmers/lev.1/subStrings.py
@@ -0,0 +1,12 @@
+
+
+
+#크기가 작은 부분문자열
+
+def solution(t, p):
+    answer = 0
+    for i in range(len(t) - len(p) + 1):
+        if int(t[i:i + len(p)]) <= int(p):
+            answer += 1
+
+    return answer
