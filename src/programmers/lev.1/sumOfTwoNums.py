05987eaca056b7e7744762dd527e9c04c8f24995
sujin <slrkajsep337@naver.com>
2023-01-23 14:11:06 +0900

    프로그래머스 - 두 개 뽑아서 더하기

--- src/programmers/lev.1/sumOfTwoNums.py
+++ src/programmers/lev.1/sumOfTwoNums.py
@@ -24,7 +24,7 @@ def solution2(numbers):
     answer = []
     for i in range(len(numbers)):
         for j in range(i + 1, len(numbers)):
-            answer.append(numbers[i] + numbers[j])함
+            answer.append(numbers[i] + numbers[j])
 
     answer = list(set(answer))
     answer.sort()
