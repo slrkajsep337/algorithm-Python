5345ca751bed19395860dd6183173a5baf39bb64
sujin <slrkajsep337@naver.com>
2023-02-20 21:07:48 +0900

    프로그래머스 - 소수 만들기

--- src/programmers/lev.1/primeNumber.py
+++ src/programmers/lev.1/primeNumber.py
@@ -20,3 +20,21 @@ def solution(nums):
 
     return result
 
+
+#다른 풀이
+
+def solution(nums):
+    #import 함수 안에 가능
+    from itertools import combinations as cb
+    answer = 0
+    for a in cb(nums, 3):
+        cand = sum(a)
+        # for~else문 -> for문이 break 없이 온전히 종료 되었을 때 else문이 실행된다.
+        # for~else문을 사용하면 check로 확인해주지 않아도 된다.
+        for j in range(2, cand):
+            if cand%j==0:
+                break
+        else:
+            answer += 1
+    return answer
+
