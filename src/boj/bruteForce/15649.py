abfd0d1c096a10d0c70f5e978d17014928dc0c49
sujin <slrkajsep337@naver.com>
2023-02-25 00:35:11 +0900

    백준 - 15649

--- src/boj/bruteForce/15649.py
+++ src/boj/bruteForce/15649.py
@@ -13,5 +13,24 @@ for p in permutations(l, m):
     sys.stdout.write("\n")
 
 
+#직접 구현 - 재귀 이용
+selected = [0 for _ in range(m)]
+used = [0 for _ in range(n + 1)] #중복을 걸러주기 위한 배열 생성
+
+def permutation(k):
+    if k == m:
+        for i in selected:
+            sys.stdout.write(str(i)+" ")
+        sys.stdout.write("\n")
+    else:
+        for i in range(1, n+1):
+            if not used[i]:
+                selected[k] = i
+                used[i] = 1
+                permutation(k+1)
+                selected[k] = 0
+                used[i] = 0
+
+permutation(0)
 
 
