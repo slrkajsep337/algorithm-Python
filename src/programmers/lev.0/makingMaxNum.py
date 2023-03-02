52977c47c8052b1ea67404400e1189562e921e6d
sujin <slrkajsep337@naver.com>
2023-03-02 11:56:55 +0900

    프로그래머스 - 최댓값 만들기 (2), 백트래킹 연습

--- src/programmers/lev.0/makingMaxNum.py
+++ src/programmers/lev.0/makingMaxNum.py
@@ -6,10 +6,9 @@
 def solution(numbers):
     temp = [0, 0]
     used = [0 for x in range(len(numbers))]
-    answer = 0
-
-    if len(numbers) == 2:
-        return numbers[0] * numbers[1]
+    #최소 값을 0으로 잡으면 원소가 음수, 양수 두개일 경우 곱이 음수가되어 비교결과 0을 최대로 출력하는 오류가 발생한다
+    #범위가 -10000~10000이므로 최대최소곱을 최소값으로 설정
+    answer = -100000000
 
     def calculate(k):
         if k == 2:
@@ -20,9 +19,16 @@ def solution(numbers):
                 if used[key] == 0:
                     temp[k] = value
                     used[key] = 1
-                calculate(k + 1)
-                temp[k] = 0
-                used[key] = 0
+                    calculate(k + 1)
+                    temp[k] = 0
+                    used[key] = 0
 
     calculate(0)
     return answer
+
+
+#combinations 이용
+def solution(numbers):
+    from itertools import combinations
+    l = [c[0]*c[1] for c in combinations(numbers, 2)]
+    return max(l)
