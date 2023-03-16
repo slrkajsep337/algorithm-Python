89fe7aec59533129fbb468e47cba29bf3fcb6147
sujin <slrkajsep337@naver.com>
2023-03-16 10:54:07 +0900

    백준 - 4963 섬의 개수

--- src/boj/bfsdfs/4963.py
+++ src/boj/bfsdfs/4963.py
@@ -1,33 +1,35 @@
-
-
 #섬의 개수
 import sys
-sys.setrecursionlimit(10**5)
 input = sys.stdin.readline
+sys.setrecursionlimit(10**5)
 
-dir = [[1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [-1, -1], [1, -1], [-1, 1]]
-
+dir = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]]
 def dfs(i, j):
-    graph[i][j] = True
-    for dx, dy in dir:
-        nx, ny = dx+i, dy+j
-        if nx<0 or nx>=h or ny<0 or ny>=w: continue
-        if graph[nx][ny] == 0 or visited[nx][ny]: continue
-
+    #현재 노드 방문 처리
+    visited[i][j] = True
+    #인접한 노드 탐색
+    for x, y in dir:
+        nx, ny = x+i, y+j
+        if nx<0 or nx>=h or ny<0 or ny>=w:
+            continue
+        if graph[nx][ny] == 0 or visited[nx][ny]:
+            continue
         dfs(nx, ny)
+
 
 while True:
     w, h = map(int, input().split())
     if w==0 and h==0: break
 
-    graph = [list(map(int, input().split())) for _ in range(h)]
-    visited = [[False]*w for _ in range(h)]
+    visited = [[False]*w for x in range(h)]
+    graph = [list(map(int, input().split())) for x in range(h)]
     answer = 0
 
     for i in range(h):
         for j in range(w):
-            if graph[i][j] != 0 and visited[i][j] == False:
-                answer += 1
-                dfs(i, j)
+            if graph[i][j] == 0 or visited[i][j]:
+                continue
+            answer += 1
+            dfs(i, j)
 
     print(answer)
