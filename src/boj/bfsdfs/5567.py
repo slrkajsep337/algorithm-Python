a59585f1d5078062c9f8f4dfad56838aa88a5a85
sujin <slrkajsep337@naver.com>
2023-03-30 12:32:39 +0900

    백준 - 결혼식

--- src/boj/bfsdfs/5567.py
+++ src/boj/bfsdfs/5567.py
@@ -3,7 +3,7 @@
 
 #결혼식
 
-#dfs 내풀이
+# dfs 내풀이
 import sys
 input = sys.stdin.readline
 
@@ -27,6 +27,33 @@ def dfs(start, depth):
 
 dfs(1, 0)
 print(sum(invited)-1)
+
+
+#이중포문, dfs랑 같은 로직 시간도 같음
+#단, for문 안에 if문 제거후 시간 조금 단축
+import sys
+input = sys.stdin.readline
+
+n = int(input()) #상근이 포함 사람수, 상근이는 1번
+m = int(input())
+relations = [[] for _ in range(n+1)]
+for i in range(m):
+    a, b = map(int, input().split())
+    relations[a].append(b)
+    relations[b].append(a)
+
+invited = [0]*(n+1)
+
+if not relations[1]: print(0)
+else:
+    invited[1] = 1
+    for i in relations[1]:
+        invited[i] = 1
+        for j in relations[i]:
+            invited[j] = 1
+
+    print(sum(invited)-1)
+
 
 
 
