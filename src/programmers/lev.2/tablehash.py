32f03547a6e3ff946aeb14fee33c79580c32c7cb
sujin <slrkajsep337@naver.com>
2023-04-11 23:28:27 +0900

    프로그래머스 - 테이블 해시 함수

--- src/programmers/lev.2/tablehash.py
+++ src/programmers/lev.2/tablehash.py
@@ -2,20 +2,20 @@
 
 #테이블 해시 함수
 
-
 def solution(data, col, row_begin, row_end):
     answer = 0
-    result = []
-
     data = sorted(data, key = lambda x:(x[col-1], -x[0]))
 
     for i in range(row_begin-1, row_end):
         temp = 0
         for j in data[i]:
             temp += j%(i+1)
-        result.append(temp)
+        answer ^= temp
 
-    for r in result:
-        answer ^= r
+    # 굳이 반복문이랑 배열 하나 더 쓸 필요 X
+    # for r in result:
+    #     answer ^= r
 
     return answer
+
+
