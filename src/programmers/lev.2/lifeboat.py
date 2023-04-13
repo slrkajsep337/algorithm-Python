f2d5fac0e738cacf82b31a70cab5595d2c3a00a8
sujin <slrkajsep337@naver.com>
2023-04-13 09:54:06 +0900

    프로그래머스 - 구명보트

--- src/programmers/lev.2/lifeboat.py
+++ src/programmers/lev.2/lifeboat.py
@@ -59,7 +59,6 @@ def solution(people, limit):
     answer = 0
 
     people = deque(sorted(people))
-    start, end = 0, len(people) - 1
 
     while len(people) > 1:
         if people[0] + people[-1] <= limit:
