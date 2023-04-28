1576515e40992fb36875b91de4fdaa5ca2548991
sujin <slrkajsep337@naver.com>
2023-04-28 09:55:40 +0900

    백준 - 2×n 타일링 2

--- src/boj/dp/11727.py
+++ src/boj/dp/11727.py
@@ -5,11 +5,12 @@
 import sys
 n = int(sys.stdin.readline())
 
-dp = [0]*n
+dp = [0]*(n+1)
 dp[0] = 1
 dp[1] = 3
 
-for i in range(2, n):
-    dp[i] = dp[i-1]+dp[i-2]*2
+if n>= 2:
+    for i in range(2, n):
+        dp[i] = (dp[i-1]+dp[i-2]*2)%10007
 
 print(dp[n-1])
