db28aeea31364fca14407c91aada23f9fbaf1ea9
sujin <slrkajsep337@naver.com>
2023-05-04 10:37:58 +0900

    프로그래머스 - 콜라츠 추측

---
+++ src/programmers/lev.1/Collatz.py
@@ -0,0 +1,21 @@
+
+
+
+
+#콜라츠 추측
+
+def solution(num):
+    answer = 0
+    while num != 1:
+        if answer == 501:
+            answer = -1
+            break
+        if num % 2 == 0:
+            num //= 2
+        else:
+            num = num * 3 + 1
+        answer += 1
+
+    return answer
+
+
